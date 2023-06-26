a55357db2f69d090f4738d6537f69b29abe720b5
Bizon15100 <rafal15100@gmail.com>
2023-06-26 18:14:58 +0200

    Adding new controller to log in our user

--- fnw-be/src/main/java/fnw_be/controller/RegistrationController.java
+++ fnw-be/src/main/java/fnw_be/controller/RegistrationController.java
@@ -4,12 +4,15 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import fnw_be.database.data.Registration;
 import fnw_be.database.repository.RegistrationRepoTemplate;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.List;
 
 
 @Controller
@@ -25,12 +28,34 @@ public class RegistrationController {
     ResponseEntity<Registration> register(@RequestBody String registrationString) throws JsonProcessingException {
         ObjectMapper mapper = new ObjectMapper();
         Registration registration = mapper.readValue(registrationString, Registration.class);
-        Registration byName = repoTemplate.findByName(registration.getName());
-        if (byName == null) {
+        Registration byEmail = repoTemplate.findByEmail(registration.getEmail());
+        if (byEmail == null) {
             repoTemplate.save(registration);
             return ResponseEntity.ok(registration);
         }
         return ResponseEntity.badRequest().build(); //Status 400
     }
+
+
+    @CrossOrigin(origins = "*")
+    @RequestMapping(value = "/login/response", method = RequestMethod.POST)
+    ResponseEntity<Registration> login(@RequestBody String loginString) throws JsonProcessingException {
+        ObjectMapper mapper = new ObjectMapper();
+        Registration registration = mapper.readValue(loginString, Registration.class);
+        Registration byEmail = repoTemplate.findByEmail(registration.getEmail());
+        Registration byPassword = repoTemplate.findByPassword(registration.getEmail());
+        if (byEmail == null && byPassword == null) {
+            return ResponseEntity.ok(registration);
+        }
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build(); //Status 401
+    }
+
+    @RequestMapping(value = "/registration/allUsers", method = RequestMethod.GET)
+    ResponseEntity<List<Registration>> getAllUsers() {
+        List<Registration> all = repoTemplate.findAll();
+
+        return ResponseEntity.ok(all);
+    }
+
 
 }
--- fnw-be/src/main/java/fnw_be/database/repository/RegistrationRepo.java
+++ fnw-be/src/main/java/fnw_be/database/repository/RegistrationRepo.java
@@ -5,4 +5,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RegistrationRepo extends JpaRepository<Registration, Long> {
     Registration findByName(String name);
+
+    Registration findByEmail(String email);
+
+    Registration findByPassword(String password);
 }
