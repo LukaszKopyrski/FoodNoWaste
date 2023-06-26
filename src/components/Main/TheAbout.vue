b315799ee12f1ae2f0382b5f48a053264a48c2f4
Rafał <58555259+Bizon15100@users.noreply.github.com>
2023-06-26 18:20:29 +0200

    Delete src directory

--- src/components/Main/TheAbout.vue
+++
@@ -1,3 +0,0 @@
-<template>
-    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Obcaecati consectetur, aperiam animi eveniet culpa aspernatur sed autem quaerat, illo tempora reiciendis neque unde sit necessitatibus itaque distinctio. Quibusdam, aliquid consequatur?
-</template>
