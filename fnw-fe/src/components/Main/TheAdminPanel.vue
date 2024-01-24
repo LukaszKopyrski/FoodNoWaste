395cec1a95ff01fd6493509a6e345e2525804e7b
Lukasz <lukasz.kopyrski98@gmail.com>
2024-01-24 20:44:01 +0100

    Adding node_modules directory to ignore

--- fnw-fe/src/components/Main/TheAdminPanel.vue
+++ fnw-fe/src/components/Main/TheAdminPanel.vue
@@ -1,17 +1,50 @@
 <template>
-    <section>
-        {{ this.$store.state.isLogged }} {{ this.$store.state.isAdmin }}
-        <div v-if="!isAdminAndIsLogged">treść tylko dla administratora</div>
-        <div v-if="isAdminAndIsLogged">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolor illo debitis consequatur corrupti! Excepturi alias corrupti odit, iusto voluptatibus laborum, ratione delectus, ipsum facilis repellat error debitis odio nostrum natus.</div>
-    </section>
+  <section v-if="isAdminAndIsLogged">
+    <article v-for="u in userData" :key="u.id">
+      <TheUserData :user="u"></TheUserData>
+    </article>
+  </section>
+  <section v-if="!isAdminAndIsLogged">
+    Treść tylko dla administratora
+  </section>
 </template>
 
+<style>
+section{
+  margin: 10px;
+}
+</style>
 <script>
-export default{
-    computed: {
+import axios from 'axios';
+import TheUserData from './TheUserData.vue';
+
+export default {
+  components: {
+    TheUserData
+  },
+  data() {
+    return {
+      userData: []
+    };
+  },
+  mounted() {
+    this.getData();
+  },
+  methods: {
+    async getData() {
+      try {
+        const response = await axios.get('http://localhost:8082/registration/allUsers');
+        this.userData = response.data;
+        console.log(this.userData)
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  },
+  computed: {
     isAdminAndIsLogged() {
       return this.$store.state.isLogged && this.$store.state.isAdmin;
-    },
-  },
-}
+    }
+  }
+};
 </script>
